6bb88d94f8fe0f6fcd0ca201bef5da6c63955af3
Dattatray Venkat Panchal <panchaldatta1508@gmail.com>
2024-03-29 18:34:52 +0530

    Added Delete Feature In Todo List

--- Todo List/src/components/TodoList.jsx
+++ Todo List/src/components/TodoList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoList = () => {
-  const  [todo, setTodo] = useState([])
+  const  [todo, setTodo] = useState([{task: "sample task" ,id:uuidv4()}])
   const [inpVal , setInpVal] = useState("");
    
   const addNewTask = () =>{
-      setTodo([...todo,inpVal]);
+      setTodo((prevTodos)=>{
+       return  [...prevTodos,{task : inpVal , id : uuidv4()}];
+
+      })
       setInpVal("");
   }
   
@@ -14,6 +18,9 @@ const TodoList = () => {
       setInpVal(event.target.value);
   }
 
+  const deleteTodo = (id) =>{
+    setTodo(todo.filter((todo)=>todo.id != id));
+  }
   return (
     <div className="flex flex-col p-[4vw] absolute left-[50%] -translate-y-[50%] -translate-x-[50%] top-[50%] w-[50%] min-h-64 bg-zinc-500">
       <input value={inpVal}  onChange={updateTodoValue}
@@ -25,10 +32,15 @@ const TodoList = () => {
       <hr />
       <div className="text-white mt-4">
         <h1 className="font-bold text-5xl mb-3 ">TASKS</h1>
-        <li>Sample Task</li>
-        {todo.map((val,i)=>{
-         return <li>{val}</li>
-        })}
+        <ul>
+
+           {todo.map((todo)=>{
+            return <li key={todo.id} className="text-white mb-3 flex items-center justify-between">
+             <span> {todo.task} </span>
+             <button onClick={()=>deleteTodo(todo.id)} className="px-4 py-1 rounded-md  bg-red-600 text-white font-bold">Delete</button>
+              </li>
+           })}
+        </ul>
       </div>
     </div>
   );
