74a8c2ec0b15359a9622ecca763412fc8023d4a9
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-03 21:46:33 +0530

    Learning The Concepts Of Forms

---
+++ Forms/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" placeholder='Enter Your Name ' />
+        </form>
+    </div>
+  )
+}
+
+export default Form
