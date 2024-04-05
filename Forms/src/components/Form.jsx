9b91d127e0786deefd973b96ac0f8c91e9e07ee1
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-05 21:41:23 +0530

    Learned About The onChange Event

--- Forms/src/components/Form.jsx
+++ Forms/src/components/Form.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Form = () => {
+  const [fullName, setfullName] = useState("");
+  let handleNameChange = (event) =>{
+    console.log(event.target.value);
+    setfullName(event.target.value)
+  }
   return (
     <div>
         <form action="">
-            <input type="text" placeholder='Enter Your Name ' />
-            <input type="submit"  />
+            <input type="text" placeholder='Enter Your Name '  value={fullName} onChange={handleNameChange}/>
+            <button>Submit</button>
         </form>
     </div>
   )
